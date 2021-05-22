7585f2bab668f4b179e85f7e3d5e89e792837623
sanket <sanketsabale9767@gmail.com>
2021-05-22 11:58:59 +0530

    persist storage added in crwn-cloth project

--- crwn-cloth/src/Redux/root-reducer.js
+++ crwn-cloth/src/Redux/root-reducer.js
@@ -1,10 +1,19 @@
 import { combineReducers } from "redux";
 import cartReducer from "./cart/cart.reducer";
 import userReducer from "./User/user.reducer";
+import storage from 'redux-persist/lib/storage';
+import persistReducer from "redux-persist/es/persistReducer";
 
+const persistConfig = {
+    key:'root',
+    storage,
+    whitelist:['cart']
+}
 
-export default combineReducers({
+const rootReducer = combineReducers({
     user : userReducer,
     cart: cartReducer
-});
+})
+
+export default persistReducer(persistConfig,rootReducer);
 
