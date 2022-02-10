da6c69ecf5387d19bf8b500341dbccf8745e4024
Sanket <ProgrammerSanket@gmail.com>
2022-02-10 07:48:56 +0530

    working done on Events and Props-drilling and FeedbackStats

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import FeedbackList from "./Components/FeedbackList";
+import FeedbackStats from "./Components/FeedbackStats";
 import Header from "./Components/Header";
 import FeedBackData from "./data/FeedbackData";
 
@@ -10,11 +11,17 @@ import FeedBackData from "./data/FeedbackData";
 
 const App = () => {
   const [feedback,setFeedback] = useState(FeedBackData);
+  const feedbackDelete = (id) => {
+    if(window.confirm('Are you shure?')){
+      setFeedback(feedback.filter(item => item.id !== id))
+    }
+  }
   return (
     <>
       <Header/>
       <div className="container">
-        <FeedbackList feedback={feedback} />
+        <FeedbackStats feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={feedbackDelete}/>
       </div>
     </>
   );
--- feedback-app/src/Components/FeedbackItem.jsx
+++ feedback-app/src/Components/FeedbackItem.jsx
@@ -1,13 +1,17 @@
 import Card from "./shared/Card";
 import PropTypes from 'prop-types';
+import { FaTimes } from 'react-icons/fa';
 
-const FeedbackItem = ({item}) => {
-   
+const FeedbackItem = ({item,handleDelete}) => {
+
     return ( 
         <Card reverse={true}>
             <div className="num-display">
                 {item.rating}
             </div>
+            <button className="close" onClick={() => handleDelete(item.id)}>
+                <FaTimes color="purple" />
+            </button>
             <div className="text-display">
                {item.text}
             </div>
