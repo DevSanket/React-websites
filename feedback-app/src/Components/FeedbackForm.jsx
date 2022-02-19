6f49db6525fc0d9571efb56cfe87f385c4802ef6
Sanket <ProgrammerSanket@gmail.com>
2022-02-19 10:54:07 +0530

    working on Handling text in Feedback-app

--- feedback-app/src/Components/FeedbackForm.jsx
+++ feedback-app/src/Components/FeedbackForm.jsx
@@ -1,9 +1,23 @@
 import { useState } from "react";
+import Button from "./shared/Button";
 import Card from "./shared/Card";
 
 const FeedBackForm = () => {
     const [text,setText] = useState('');
+    const [btnDisabled, setBtnDisabled] = useState(true);
+    const [message,setMessage] = useState('');
+
     const handleTextChange = (e) => {
+        if(text === ''){
+            setBtnDisabled(true);
+            setMessage(null);
+        }else if(text !== '' && text.trim().length <= 10){
+            setBtnDisabled(true);
+            setMessage('You need atleast 10 characters');
+        }else{
+            setBtnDisabled(false);
+            setMessage(null);
+        }
         setText(e.target.value);
     }
     return ( 
@@ -16,8 +30,9 @@ const FeedBackForm = () => {
                     onChange={handleTextChange}
                     value={text}
                     type="text" placeholder="Write a review" />
-                    <button type="submit">Send</button>
+                    <Button type="submit" isDisabled={btnDisabled}>Send</Button>
                 </div>
+                {message && <div className="message">{message}</div>}
             </form>
         </Card>
      );
